219c8d68424a2d847a2eea55ad606c6396f98839
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-31 13:36:45 +0200

    React Routes and Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +14,12 @@ function App() {
     <>
       <h1>REACT ROUTER DOM</h1>
       {/* https://www.freecodecamp.org/news/how-to-use-react-router-version-6/ */}
+      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
     </>
   )
 }
