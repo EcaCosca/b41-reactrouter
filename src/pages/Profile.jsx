ebb8f216a2e2a3e0dbcf3dd246c2d299a5a07388
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-31 14:32:59 +0200

    DONE

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -18,23 +18,10 @@ const Profile = () => {
 
       <h1>{product.title}</h1>
       <h2>$: {product.price}</h2>
-      <img src={product.img}/>
+      <img src={product.image}/>
       <p>{product.description}</p>
     </>
   )
 }
 
 export default Profile
-
-// {
-//   "id": 1,
-//   "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-//   "price": 109.95,
-//   "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-//   "category": "men's clothing",
-//   "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-//   "rating": {
-//   "rate": 3.9,
-//   "count": 120
-//   }
-//   }
