df1ce7cbae17ac5b7d205f0dc54d6595df487040
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-31 14:16:03 +0200

    React router dom lecture done

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,23 @@ import './App.css'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import About from './pages/About'
-import {Route, Routes, Link} from 'react-router-dom'
+import {Route, Routes, Link, NavLink, useNavigate} from 'react-router-dom'
 
 function App() {
+  const navigate = useNavigate()
+
+  const navStyle = {
+    fontWeight: "bold",
+    color: "red"
+  }
+
+  const handleClick = () => {
+    navigate(-1);
+  }
+
+  const goBackHome = () => {
+    navigate('/');
+  }
 
   return (
     <>
@@ -16,16 +30,76 @@ function App() {
       <div className='nav'>
         <ul>
           {/* LINK    */}
-          <li><Link to="/">Home</Link></li>
+          {/* <li><Link to="/">Home</Link></li>
           <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/about">About</Link></li>
+          <li><Link to="/about">About</Link></li> */}
+          {/* NAVLINK    */}
+          {/* <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/profile">Profile</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li> */}
+          <li>
+            <NavLink
+              to="/"  
+              style={({ isActive }) => {
+                return {
+                  fontWeight: isActive ? "bold" : "",
+                  color: isActive ? "red" : "black",
+                };
+              }}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+          <NavLink
+            to="/profile/123"  
+            style={({ isActive }) => {
+              return {
+                fontWeight: isActive ? "bold" : "",
+                color: isActive ? "red" : "black",
+              };
+            }}
+          >
+            Profile 123
+          </NavLink>
+          </li>
+          <li>
+          <NavLink
+            to="/profile/555"  
+            style={({ isActive }) => {
+              return {
+                fontWeight: isActive ? "bold" : "",
+                color: isActive ? "red" : "black",
+              };
+            }}
+          >
+            Profile 555
+          </NavLink>
+          </li>
+          <li>
+          <NavLink
+            to="/about"  
+            style={({ isActive }) => {
+              return {
+                fontWeight: isActive ? "bold" : "",
+                color: isActive ? "red" : "black",
+              };
+            }}
+          >
+            About
+          </NavLink>
+          </li>
         </ul>
       </div>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/:userId' element={<Profile />} />
       </Routes>
+
+      <button onClick={handleClick}>GO BACK</button>
+      <br/>
+      <button onClick={goBackHome}>GO BACK HOME</button>
     </>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Profile = () => {
+  let { userId } = useParams();
+
+  console.log(userId)
+  
   return (
-    <h1>Profile</h1>
+    <h1>Profile for user {userId}</h1>
   )
 }
 
