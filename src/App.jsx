97bba4b91bc87db622852cb484b4adbcf603d399
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-31 13:47:23 +0200

    Link EXPLAINED

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,22 @@ import './App.css'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import About from './pages/About'
-import {Route, Routes} from 'react-router-dom'
+import {Route, Routes, Link} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>REACT ROUTER DOM</h1>
       {/* https://www.freecodecamp.org/news/how-to-use-react-router-version-6/ */}
-      
+      <div className='nav'>
+        <ul>
+          {/* LINK    */}
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </div>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
